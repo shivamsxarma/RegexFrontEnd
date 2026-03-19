225583c60457ab56bc82124d6c106e433d75c536
shivam sharma <shivamsxarma@gmail.com>
2026-03-19 13:50:01 +0530

    new commit

--- JavaScript_basics/day10.js
+++ JavaScript_basics/day10.js
@@ -1,18 +1,26 @@
 // console.log("hellow")
 // closures
- function outer(){
-count = 0;
-return function inner (){
+//  function outer(){
+// count 
+// return function inner (){
     
-    count++;
-    return count ;
-}
-}
+//     count++;
+//     return count ;
+// }
+// }
 
-const counter = outer();
-console.log(counter());
-console.log(counter());
-console.log(counter());
-console.log(counter());
+// const counter = outer();
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
+// console.log(counter());
 
 // console.log(typeof  )
+
+
+ let a = 2;
+a++
+console.log(a)
+console.log(a)
+console.log(a)
+  console.log(a);
---
+++ JavaScript_basics/day11.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a = 0;
+    a = ++a;
+    cout<<a;
+    cout<<a;
+    cout<<a;
+    cout<<a;
+    for(int i =0;i<=10;i++)
+    {
+        cout<<++i<<endl;
+    }
+    cout<<"hellow";
+  
+}

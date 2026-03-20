9fa27c1c9d20cf5d47a8c28b91c6eb1b7121da1a
shivam sharma <shivamsxarma@gmail.com>
2026-03-20 15:24:35 +0530

    hii new commit

--- JSAssignments/day2.js
+++ JSAssignments/day2.js
@@ -216,13 +216,14 @@
 //  20. Variable Shadowing in Blocks 
  
  
-let x = 5; 
-{ 
-  let x = 10; 
-  console.log(x); 
-} 
-console.log(x); 
+// let x = 5; 
+// { 
+//   let x = 10; 
+//   console.log(x); 
+// } 
+// console.log(x); 
  
+// output-10,5
 
  
 

721579747bcd11b16566d6c11b7661004f51e3f2
shivam sharma <shivamsxarma@gmail.com>
2026-03-24 13:18:31 +0530

    new update

--- JavaScript_basics/day12.js
+++ JavaScript_basics/day12.js
@@ -1,12 +1,22 @@
-function empobj(...rest){
-    return { 
-        Firstname,
-        lastname,
-        DOJ,
+// function empobj(...rest){
+//     return { 
+//         Firstname,
+//         lastname,
+//         DOJ,
 
 
-    }
+//     }
  
+// }
+//    let emp1 =  empobj('"shivam"',"setDefaultHighWaterMark","9900")
+//     console.log(emp1)
+
+let emp = {
+    firstname:"shivam",
+    lastname:"sharma",
+    age:23,
+    getdata:function(){
+        console.log(this.firstname)
+    }
 }
-   let emp1 =  empobj('"shivam"',"setDefaultHighWaterMark","9900")
-    console.log(emp1)
+getdata()

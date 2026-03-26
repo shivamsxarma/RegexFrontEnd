b3a30932750f727f595c84008093579ad1b5bdd9
shivam sharma <shivamsxarma@gmail.com>
2026-03-26 12:31:37 +0530

    new update

--- JavaScript_basics/callbacks.js
+++ JavaScript_basics/callbacks.js
@@ -23,3 +23,22 @@
 // console.log(num)                
 
 //callback : function accepcting another function as an argument  are called an call back function
+
+// or is chij ko higher order function kehte haii 
+
+//Calculator Example of call back function 
+// sum mul divide
+function sum(a, b) {
+  return a + b;
+}
+function sub(a, b) {
+  return b - a;
+}
+function Calculator(a, b, operation) {
+  return operation(a, b);
+}
+let res1 = Calculator(3, 5, sum);
+console.log(res1);
+
+let res2 = Calculator(4, 3, sub);
+console.log(res2);
